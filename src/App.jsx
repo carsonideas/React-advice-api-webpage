dfd23cd313ad22acdb4d6108b95603d4dfd9c304
Ideahub <douglascarson1121@gmail.com>
2025-06-24 10:45:14 +0300

    add picture and design on the webpage such as advice number

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,16 @@ function App() {
   const [advice, setAdvice] = useState("");
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [id, setId] = useState(null);
+  const [picture, setPicture] = useState(null);
 
   async function handleGetAdvice(e) {
     e.preventDefault();
     setLoading(true);
     setError(null);
     setAdvice("");
-
+    setAdvice("");
+    setPicture("picture");
     try {
       const response = await fetch("https://api.adviceslip.com/advice");
       if (!response.ok) {
@@ -21,6 +24,7 @@ function App() {
       }
       const data = await response.json();
       setAdvice(data.slip.advice);
+      setId(data.slip.id);
     } catch (e) {
       setError("HOUSTON!! Something went wrong. Please try again...nooo");
     } finally {
@@ -30,6 +34,8 @@ function App() {
 
   return (
     <div className="app">
+      {/* <div className="myadvisepicture"><img src="./image.png" alt="" /></div> */}
+      {picture && <p className="myadvisepicture"><img src="/good-advice-concept-little-d-man-rolling-down-reader-45526119.jpg.webp" alt="" /></p>}
       <form onSubmit={handleGetAdvice}>
         <button type="submit" disabled={loading}>
           Get Random Advice
@@ -38,9 +44,14 @@ function App() {
 
       {loading && <SyncLoader color="green" size={15} />}
 
+      {id && <p className="myid">Advice {id}</p>}
+
       {advice && <p className="result">"{advice}"</p>}
 
       {error && <p className="error">{error}</p>}
+      
+      
+
     </div>
   );
 }
